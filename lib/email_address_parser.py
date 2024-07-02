9b4d3f0793435a9de21e01491edd6744f802d18c
Taylor Ng <taylormng95@gmail.com>
2024-07-02 18:20:27 -0400

    completes regex lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,10 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, string):
+        self.string = string
+
+    def parse(self):
+        filtered = re.compile(r'\w+\.?\w+@\w+\.[A-z]+').findall(self.string)
+        st = set(filtered)
+        return sorted(list(st))
